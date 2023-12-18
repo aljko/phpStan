62475210d14d0c2076527779b953f61ddfebd2c8
Aljo KALAC <akalac@sdv.fr>
2023-12-18 09:31:01 +0100

    ze

--- stan.php
+++ stan.php
@@ -1,10 +1,12 @@
-<?php
-	echo("fioea");
-	function aljo(){
-		return 'aljo!';
+<?php declare(strict_types = 1);
+
+	class HelloWorld
+	{
+    		/**
+     		* @return never-return
+     		*/
+    		public function encode(string $foo): array
+    		{
+        		throw new BadMethodCallException();
+    		}
 	}
-echo("fioea");
-echo("fioea");
-echo("fioea");
-echo("fioea");
-echo("fioea");
