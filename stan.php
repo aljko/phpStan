6e27234b50423d99623263cc85bc5cc6701a79f4
Aljo KALAC <akalac@sdv.fr>
2023-12-19 09:09:25 +0100

    régression

--- stan.php
+++ stan.php
@@ -4,6 +4,10 @@ class helloWorld
 {
     public function aljo(string $AaaAaa): string
     {
+        $array = [
+            1 => 'aljo',
+            2 => 'kalac'
+        ];
         return $AaaAaaa;
     }
 }
