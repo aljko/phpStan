60f88e3589f2f636329cb70fc54348ae4e79389d
Aljo KALAC <akalac@sdv.fr>
2023-12-20 08:59:36 +0100

    Undefined array key person

--- stan.php
+++ stan.php
@@ -1,6 +1,10 @@
-<?php declare(strict_types = 1);
+<?php
 
-class helloWorld
+declare(strict_types=1);
+
+namespace foo;
+
+class HelloWorld
 {
     public function aljo(string $AaaAaa): string
     {
